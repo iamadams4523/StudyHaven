a4f2b73c8dfef2208d480c6d5180899454897936
iamadams <iamadams4523@gmail.com>
2025-03-25 14:49:18 +0100

    Ignored eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Skips ESLint errors on build
+  },
+  /* Add other config options here */
 };
 
 export default nextConfig;
